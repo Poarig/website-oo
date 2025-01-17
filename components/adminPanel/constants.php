441d3053d8e347e472c958d81e5ad16e4c727ae5
Poarig <poarig@yandex.ru>
2025-01-17 09:54:45 +0300

    прошлогодние изменения

---
+++ components/adminPanel/constants.php
@@ -0,0 +1,14 @@
+<?php
+$MATCHES_TABLE_AND_ID = array(
+    "respondent_id" => ["Users", "user_id", ["employee_id"]],
+    "last_modified_user_id" => ["Users", "user_id", ["employee_id"]],
+    "specialties_id"=> ["Specialties", "specialtie_id", ["specialties_name"]],
+    "curriculum_file_id" => ["Files", "file_id", ["file_name"]],
+    "work_programms_page_id"=> ["Pages", "page_id", ["page_url"]],
+    "calendar_schedule_file_id"=> ["Files", "file_id", ["filename"]],
+    "metodological_documents_page_id"=> ["Pages", "page_id", ["page_url"]],
+    "annotations_page_id"=> ["Pages", "page_id", ["page_url"]],
+    "description_file_id"=> ["Files", "file_id", ["file_name"]],
+    "employee_id"=> ["Employees", "employee_id", ["employee_name", "employee_surname", "employee_patronymic"]],
+)
+?>
--- pages/adminGeneratedTable.php
+++ pages/adminGeneratedTable.php
@@ -6,33 +6,71 @@ if (!isset($_SESSION["auth"])){
 include("../components/head.php");
 include("../components/adminPanel/adminHeader.php");
 include("../components/adminPanel/adminNav.php");
+include("../components/connection.php");
+include("../components/adminPanel/adminTable.php");
+include("../components/adminPanel/constants.php");
+
+function MatchTableAndID($conn, $matches, $table_name) {
+    $columns_names_sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$table_name'";
+    if ($columns_names_result = mysqli_query($conn, $columns_names_sql)){
+        $columns_names = [];
+        $other_table_colums = [];
+
+        foreach($columns_names_result as $row) {
+            $column_name = $row['COLUMN_NAME'];
+            $column_name_len = strlen($column_name);
+
+            if ((count($columns_names) == 0) or ($column_name_len <= 3)){
+                array_push($columns_names, $column_name);
+            }else if (substr($column_name, $column_name_len -3, 3) != "_id") {
+                array_push($columns_names, $column_name);
+            }else{
+                array_push($other_table_colums, [$table_name, $column_name, $matches[$column_name][0], $matches[$column_name][1]]);
+                $other_table_cloumns_names = [];
+                foreach ($matches[$column_name][2] as $name){
+                    if (strlen($name) > 3){
+                        if (substr($column_name, $column_name_len -3, 3) == "_id") {
+                            $other_table_cloumns_names = $other_table_cloumns_names + $matches[$name][2];
+                            array_push($other_table_colums, [$matches[$column_name][0], $name, $matches[$name][0], $matches[$name][1]]);
+                        }
+                    }
+                    array_push($other_table_cloumns_names, $column_name."-".explode("-", $name)[0]);
+                }
+                $columns_names = $columns_names + $other_table_cloumns_names;
+            }
+            
+
+        }
+
+        return [$columns_names, $other_table_colums];
+    }
+}
 ?>
 <div class="main admin-panel">
     <div class="main-content m-w-n">
         <?php
-        include("../components/connection.php");
-        $sql='SELECT * FROM `' . $_GET['table'] . "`";
-        if ($result = mysqli_query($conn, $sql)) {
-            $columns_names_sql = 'SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "' . $_GET['table'] . '"';
-            $columns_names_result = mysqli_query($conn, $columns_names_sql);
-            $columns_names=[];
-            foreach($columns_names_result as $row) {
-                array_push($columns_names, $row['COLUMN_NAME']);
-            }
 
+        $columns = MatchTableAndID($conn, $MATCHES_TABLE_AND_ID, $_GET['table']);
+        $columns_names = implode(", ", $columns[0]);
+        $sql="SELECT {$columns_names} FROM {$_GET['table']}";
+        foreach ($columns[1] as $other_table_column) {
+            $sql = "$sql INNER JOIN {$other_table_column[0]} ON {$other_table_column[0]}.{$other_table_column[2]} = {$other_table_column[1]}.{$other_table_column[3]}";
+        }
+        if ($result = mysqli_query($conn, $sql)) {
             $td = [];
             foreach($result as $row) {
                 array_push($td, $row);
-            }
+                }
+
             mysqli_free_result($result);
 
             $data = array(
                 'caption' => $_GET['table'],
-                'th' => $columns_names,
+                'th' => $columns[0],
                 'td' => $td,
             );
 
-            include("../components/adminPanel/adminTable.php");
+            
             make_table($data);
 
         } else {
