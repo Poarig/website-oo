37ee971f53f1673714c1d9f769c1467c7a49194f
Poarig <poarig@yandex.ru>
2025-01-17 11:41:10 +0300

    продолжил работу над БД

--- pages/adminGeneratedTable.php
+++ pages/adminGeneratedTable.php
@@ -11,25 +11,31 @@ include("../components/adminPanel/adminTable.php");
 include("../components/adminPanel/constants.php");
 
 function MatchTableAndID($conn, $matches, $table_name) {
+    #   получаем имена столбцов таблици
     $columns_names_sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$table_name'";
     if ($columns_names_result = mysqli_query($conn, $columns_names_sql)){
         $columns_names = [];
         $other_table_colums = [];
 
+        #   перебираем имена столбцов
         foreach($columns_names_result as $row) {
             $column_name = $row['COLUMN_NAME'];
             $column_name_len = strlen($column_name);
 
+            #   проверяем является ли столбец ссылкой на другую таблицу
             if ((count($columns_names) == 0) or ($column_name_len <= 3)){
                 array_push($columns_names, $column_name);
             }else if (substr($column_name, $column_name_len -3, 3) != "_id") {
                 array_push($columns_names, $column_name);
-            }else{
+            }else if (substr($column_name, 0, $column_name_len -4) != strtolower($table_name)){
                 array_push($other_table_colums, [$table_name, $column_name, $matches[$column_name][0], $matches[$column_name][1]]);
-                $other_table_cloumns_names = [];
+
+                # перебираем нужные имена столбцов из сылаемой таблици
                 foreach ($matches[$column_name][2] as $name){
+                    $other_table_cloumns_names = [];
                     if (strlen($name) > 3){
-                        if (substr($column_name, $column_name_len -3, 3) == "_id") {
+
+                        if (substr($name, strlen($name) -3, 3) == "_id") {
                             $other_table_cloumns_names = $other_table_cloumns_names + $matches[$name][2];
                             array_push($other_table_colums, [$matches[$column_name][0], $name, $matches[$name][0], $matches[$name][1]]);
                         }
@@ -42,7 +48,16 @@ function MatchTableAndID($conn, $matches, $table_name) {
 
         }
 
-        return [$columns_names, $other_table_colums];
+        $new_other_table_colums=[];
+        foreach($other_table_colums as $column){
+            $condition = "{$column[0]}.{$column[1]} = {$column[2]}.{$column[3]}";
+            if (!(array_key_exists($column[2], $new_other_table_colums))){
+                $new_other_table_colums[$column[2]] = [$condition];
+            } else {
+                array_push($new_other_table_colums[$column[2]], $condition);
+            }
+        }
+        return [$columns_names, $new_other_table_colums];
     }
 }
 ?>
@@ -53,9 +68,11 @@ function MatchTableAndID($conn, $matches, $table_name) {
         $columns = MatchTableAndID($conn, $MATCHES_TABLE_AND_ID, $_GET['table']);
         $columns_names = implode(", ", $columns[0]);
         $sql="SELECT {$columns_names} FROM {$_GET['table']}";
-        foreach ($columns[1] as $other_table_column) {
-            $sql = "$sql INNER JOIN {$other_table_column[0]} ON {$other_table_column[0]}.{$other_table_column[2]} = {$other_table_column[1]}.{$other_table_column[3]}";
+        foreach ($columns[1] as $key => $other_table_column) {
+            $condition = implode(" AND ", $other_table_column);
+            $sql = "$sql INNER JOIN {$key} ON {$condition}";
         }
+        print($sql);
         if ($result = mysqli_query($conn, $sql)) {
             $td = [];
             foreach($result as $row) {
